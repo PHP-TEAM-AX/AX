81645f1ada447d837a386c1d56b6d766ffce074f
TEAM AX <68008587+PHP-TEAM-AX@users.noreply.github.com>
2020-07-08 16:53:11 +0300

    Update update.php

--- tg/update.php
+++ tg/update.php
@@ -2,7 +2,7 @@
 
 $Token = "Token"; //your add Token
 $sudo = "id sudo"; //your add id sudo
-
+mkdir("tg");
 $ok = json_decode(file_get_contents("https://api.telegram.org/bot" . $Token . "/getme"),true)['ok'];
 
 $done = ["Token"=>$Token,"sudo"=>$sudo];
@@ -10,7 +10,7 @@ $done = ["Token"=>$Token,"sudo"=>$sudo];
 $error = ["Token"=>null,"sudo"=>null];
 
 if($ok == true ){
-   $put = file_put_contents("info.json",json_encode($done));
+   $put = file_put_contents("tg/info.json",json_encode($done));
 	echo " done : Was lifted Data";
 }else{
 	$put = file_put_contents("info.json",json_encode($error));
